0718f708bc53aec479867dc344af5cf76f754c6e
sdfwefk11 <mknhsad3398@naver.com>
2023-04-27 20:15:05 +0900

    Draggable

--- src/Dnd.tsx
+++ src/Dnd.tsx
@@ -5,13 +5,23 @@ function Dnd() {
     <DragDropContext onDragEnd={onDragEnd}>
       <div>
         <Droppable droppableId="01">
-          {() => (
-            <ul>
+          {(provided) => (
+            <ul ref={provided.innerRef} {...provided.droppableProps}>
               <Draggable draggableId="001" index={0}>
-                {() => <li>01</li>}
+                {(provided) => (
+                  <li ref={provided.innerRef} {...provided.draggableProps}>
+                    <span {...provided.dragHandleProps}>O</span>
+                    01
+                  </li>
+                )}
               </Draggable>
               <Draggable draggableId="002" index={1}>
-                {() => <li>02</li>}
+                {(provided) => (
+                  <li ref={provided.innerRef} {...provided.draggableProps}>
+                    <span {...provided.dragHandleProps}>O</span>
+                    02
+                  </li>
+                )}
               </Draggable>
             </ul>
           )}
